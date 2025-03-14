cdcc48d7002039fcccb2fba6cfc4cde48861076e
Vasiliy Samarin <v.samarin@kt.team>
2025-03-14 13:49:23 +0700

    hw-3-fixes-3

--- src/hooks/use-counter.jsx
+++ src/hooks/use-counter.jsx
@@ -1,26 +1,28 @@
 import React, { useState } from 'react'
 
-
-const incrementCounter = (counterValue, setCounterValue) => {
-   if (counterValue < 5) {
-      setCounterValue(counterValue + 1)
-   }
-}
-
-const decrementCounter = (counterValue, setCounterValue) => {
-   if (counterValue > 0) {
-      setCounterValue(counterValue - 1)
-   }
-}
+const max = 5
+const min = 0
 
 export const UseCounter = () => {
    const [counterValue, setCounterValue] = useState(0)
 
+   const incrementCounter = () => {
+      if (counterValue < max) {
+         setCounterValue(counterValue + 1)
+      }
+   }
+
+   const decrementCounter = () => {
+      if (counterValue > min) {
+         setCounterValue(counterValue - 1)
+      }
+   }
+
    return (
       <div className='counter'>
-         <button className='counter__btn' onClick={() => decrementCounter(counterValue, setCounterValue)}>-</button>
+         <button className='counter__btn' onClick={decrementCounter}>-</button>
          <div className='counter__btn'>{counterValue}</div>
-         <button className='counter__btn' onClick={() => incrementCounter(counterValue, setCounterValue)}>+</button>
+         <button className='counter__btn' onClick={incrementCounter}>+</button>
       </div>
    )
 }
